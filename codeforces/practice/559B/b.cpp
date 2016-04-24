21aaa58d4dab209bd309e43dc9750e0787a83d03
Farbod Motlagh <farbod.motlagh@gmail.com>
2016-04-24 22:55:35 +0930

    559b

---
+++ codeforces/practice/559B/b.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+bool isEqual(string a, string b) {
+  if (a.size() % 2 != 0) {
+    return a == b;
+  } else {
+    string a1 = a.substr(0, a.length()/2);
+    string a2 = a.substr((a.length()/2),a.length()/2);
+    string b1 = b.substr(0, b.length()/2);
+    string b2 = b.substr((b.length()/2),b.length()/2);
+    if ((isEqual(a1,b1) && isEqual(a2,b2)) || (isEqual(a1,b2) && isEqual(a2,b1))) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+int main() {
+  string s1, s2;
+  cin >> s1 >> s2;
+  if (isEqual(s1, s2)) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+
+  return 0;
+}
