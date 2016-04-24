0eb43b1d19939986e66807ab22ce2121e2bf397e
Farbod Motlagh <farbod.motlagh@gmail.com>
2016-04-24 23:10:22 +0930

    moddifed 559

--- codeforces/practice/559B/b.cpp
+++ codeforces/practice/559B/b.cpp
@@ -3,23 +3,21 @@ using namespace std;
 typedef long long ll;
 
 bool isEqual(string a, string b) {
+  if (a==b) return true;
   if (a.size() % 2 != 0) {
     return a == b;
   } else {
     string a1 = a.substr(0, a.length()/2);
-    string a2 = a.substr((a.length()/2),a.length()/2);
+    string a2 = a.substr(a.length()/2,a.length()/2);
     string b1 = b.substr(0, b.length()/2);
-    string b2 = b.substr((b.length()/2),b.length()/2);
-    if ((isEqual(a1,b1) && isEqual(a2,b2)) || (isEqual(a1,b2) && isEqual(a2,b1))) {
-      return true;
-    } else {
-      return false;
-    }
+    string b2 = b.substr(b.length()/2,b.length()/2);
+    return ((isEqual(a1,b1) && isEqual(a2,b2)) || (isEqual(a1,b2) && isEqual(a2,b1)));
   }
 }
 
 int main() {
   string s1, s2;
+  ios_base::sync_with_stdio(false);
   cin >> s1 >> s2;
   if (isEqual(s1, s2)) {
     cout << "YES" << endl;
